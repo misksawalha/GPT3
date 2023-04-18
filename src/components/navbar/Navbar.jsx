35bfbf56258b9e97405bb34d2a10aaefbfb809fb
misk <misksawallha@gmail.com>
2023-04-18 22:58:36 +0300

    feature section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   const Menu = () => (
     <>
       <p><a href='#home'>Home</a></p>
-      <p><a href='#wgbt3'>What is GPT3?</a></p>
+      <p><a href='#wgpt3'>What is GPT3?</a></p>
       <p><a href='#possibility'>Open AI</a></p>
       <p><a href='#feature'>Case study</a></p>
       <p><a href='#blog'>Library</a></p>
